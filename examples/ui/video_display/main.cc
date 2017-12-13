06b78c76c59454397bab7cb68956498ef8fc0fc1
Garratt Gallagher <garratt@google.com>
2017-12-13 15:56:50 -0800

    starting to work on video stuff
    Change-Id: I2079494981ee5a3ddc1e1fdf79a928d2df64954c

---
+++ examples/ui/video_display/main.cc
@@ -0,0 +1,29 @@
+// Copyright 2017 The Fuchsia Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <trace-provider/provider.h>
+
+#include "garnet/examples/ui/shadertoy/client/view.h"
+#include "lib/fsl/tasks/message_loop.h"
+#include "lib/fxl/command_line.h"
+#include "lib/fxl/log_settings_command_line.h"
+#include "lib/ui/view_framework/view_provider_app.h"
+
+int main(int argc, const char** argv) {
+  auto command_line = fxl::CommandLineFromArgcArgv(argc, argv);
+  if (!fxl::SetLogSettingsFromCommandLine(command_line))
+    return 1;
+
+  fsl::MessageLoop loop;
+  trace::TraceProvider trace_provider(loop.async());
+
+  mozart::ViewProviderApp app([](mozart::ViewContext view_context) {
+    return std::make_unique<shadertoy_client::View>(
+        view_context.application_context, std::move(view_context.view_manager),
+        std::move(view_context.view_owner_request));
+  });
+
+  loop.Run();
+  return 0;
+}
