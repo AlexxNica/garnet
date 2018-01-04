e1a7be11199377f3e4ceb7df837dcda91ee81559
Garratt Gallagher <garratt@google.com>
2018-01-03 16:14:51 -0800

    cleaned up, mostly compiling
    Change-Id: I3ef7797e7194f9ba26240f0893c38dce864882a5

--- examples/ui/video_display/view.h
+++ examples/ui/video_display/view.h
@@ -12,40 +12,54 @@
 #include "lib/fxl/macros.h"
 #include "lib/ui/scenic/client/resources.h"
 #include "lib/ui/view_framework/base_view.h"
+#include "garnet/examples/ui/video_display/buffer.h"
+#include <deque>
+#include <list>
 
 namespace video_display {
 
-struct Framebuffer {
-    Framebuffer  wait_(fsl::MessageLoop::GetCurrent()->async(),
-            buffer->release_fence().get(), ZX_EVENT_SIGNALED) {
-    wait_.set_handler(fbl::BindMember(this, &BufferHandler::Handler));
-  // Signaled by Renderer when frame is finished, and therefore ready for the
-  // ImagePipe consumer to use.
-  zx::event acquire_fence;
-  // Signaled by the ImagePipe consumer when the framebuffer is no longer used
-  // and can therefore be rendered into.
-  zx::event release_fence;
-  uint32_t image_pipe_id = 0;
-  uint32_t buffer_size = 0;
-  uint64_t buffer_offset = 0;
-  zx::vmo buffer;
-  // Sets up the frame buffer with everything it needs to  give access to the
-  // buffer
-  zx_status_t Create(uint32_t size, uint32_t id, uint64_t offset, zx:vmo main_buffer);
-
-  void AddToImagePipe(scenic::ImagePipePtr &image_pipe, const scenic::ImageInfo &info);
-
-  async::AutoWait wait_;
-  bool IsAvailable() { 
+struct BufferLayout {
+  uint64_t vmo_offset;
+  uint32_t width, height, bpp;
 };
 
 
+class FrameScheduler {
+  uint64_t presentation_interval_ns_ = 33000000;
+  uint64_t last_presentation_time_ns_ = 0;
+  std::deque<uint64_t> requested_presentation_times_;
+ public:
+  // Get the next frame presentation time
+  uint64_t GetNextPresentationTime() {
+    last_presentation_time_ns_ += presentation_interval_ns_;
+    requested_presentation_times_.push_back(last_presentation_time_ns_);
+    return last_presentation_time_ns_;
+  }
+
+  void Update(uint64_t presentation_time, uint64_t presentation_interval) {
+    if (requested_presentation_times_.size() == 0) {
+      return;
+    } // todo: add lots of errors
+    int64_t diff = presentation_time - requested_presentation_times_.front();
+    requested_presentation_times_.pop_front();
+    if (diff > 0) {
+      // we are behind - we need to advance our presentation timing
+      last_presentation_time_ns_ += diff;
+    } // todo error if < 0
+    presentation_interval_ns_ = presentation_interval;
+  }
+
+};
+
+
+using BufferNotifier  = fbl::Function<void(uint32_t index)>;
 
 class BufferHandler {
  public:
-  BufferHandler(Buffer *buffer, uint32_t index) :
-      buffer_(buffer),
+  BufferHandler(Buffer *buffer, uint32_t index, BufferNotifier &notifier) :
+      // buffer_(buffer),
       index_(index),
+      notifier_(fbl::move(notifier)),
       wait_(fsl::MessageLoop::GetCurrent()->async(),
             buffer->release_fence().get(), ZX_EVENT_SIGNALED) {
     wait_.set_handler(fbl::BindMember(this, &BufferHandler::Handler));
@@ -54,42 +68,26 @@ class BufferHandler {
   }
 
   ~BufferHandler() = default;
-
+  
+  // This function is called when the release fence is signalled
   async_wait_result_t Handler(async_t* async, zx_status_t status,
                               const zx_packet_signal* signal) {
       if (status != ZX_OK) {
         FXL_LOG(ERROR) << "BufferHandler received an error ("
                        << zx_status_get_string(status) << ").  Exiting.";
         wait_.Cancel();
-        fsl::MessageLoop::GetCurrent()->PostQuitTask();
         return ASYNC_WAIT_FINISHED;
       }
-
-      buffer_->Reset();
-
-      auto acq = fidl::Array<zx::event>::New(1);
-      auto rel = fidl::Array<zx::event>::New(1);
-      buffer_->dupAcquireFence(&acq.front());
-      buffer_->dupReleaseFence(&rel.front());
-
-      image_pipe->PresentImage(index_, 0, std::move(acq), std::move(rel),
-                               [](scenic::PresentationInfoPtr info) {});
-
-      uint8_t r, g, b;
-      hsv_color(hsv_index, &r, &g, &b);
-      hsv_index = hsv_inc(hsv_index, 3);
-      buffer_->Fill(r, g, b);
-
-      buffer_->Signal();
+      notifier_(index_);
       return ASYNC_WAIT_AGAIN;
   }
 
  private:
-  Buffer *buffer_;
+  // Buffer *buffer_;
   uint32_t index_;
+  BufferNotifier notifier_;
   async::AutoWait wait_;
 };
-
 
 class View : public mozart::BaseView {
  public:
@@ -102,28 +100,41 @@ class View : public mozart::BaseView {
   // mozart::BaseView.
   virtual bool OnInputEvent(mozart::InputEventPtr event) override;
 
+// When a buffer is released, signal that it is available to the writer
+// In this case, that means directly write to the buffer then re-present it
+void BufferReleased(uint32_t  buffer_index);
+
+void OnFramePresented(const scenic::PresentationInfoPtr& info);
+
+// When an incoming buffer is filled, View releases the aquire fence
+void IncomingBufferFilled(uint32_t buffer_index);
+
+// called to reserve a buffer for writing
+void ReserveIncomingBuffer(uint32_t buffer_index);
+
+zx_status_t SetupBuffers(const std::vector<BufferLayout> &buffer_layouts, 
+                   const zx::vmo &vmo);
  private:
   // |BaseView|.
-  void OnSceneInvalidated(
-      scenic::PresentationInfoPtr presentation_info) override;
+  // void OnSceneInvalidated(
+      // scenic::PresentationInfoPtr presentation_info) override;
 
-  app::ApplicationContext* const application_context_;
+  // app::ApplicationContext* const application_context_;
   fsl::MessageLoop* loop_;
 
   scenic_lib::ShapeNode node_;
-  // std::vector<scenic_lib::ShapeNode> nodes_;
 
   // Image pipe to send to display
   scenic::ImagePipePtr image_pipe_;
 
-  static constexpr uint32_t kNumFramebuffers = 5;
-  Framebuffer frame_buffers_[kNumFramebuffers];
-  uint32_t buffer_position = 0;
+  std::vector<Buffer*> frame_buffers_;
+  std::list<BufferHandler> frame_handlers_; // todo: this can't be resized...
+  // uint32_t buffer_position = 0;
   zx::vmo vmo_;
-
-  zx_time_t frame_start_time_ = 0;
+  FrameScheduler frame_scheduler_;
+  // zx_time_t frame_start_time_ = 0;
 
   FXL_DISALLOW_COPY_AND_ASSIGN(View);
 };
 
-}  // namespace shadertoy_client
+}  // namespace video_display
